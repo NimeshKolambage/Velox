62ad51b1e4142859194250e2f2a7e63235eec576
NimeshKolambage <nimeshkolambage@gmail.com>
2026-04-20 00:13:38 +0530

    add project setup and gitignore files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import authRoutes from './auth.route.js';
 const app = express();
+
+app.use("/api/auth",authRoutes);
 
 app.listen(3005, () => {
   console.log('Server is running on port 3005');
