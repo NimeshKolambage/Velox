f5d1a976bc1bbc71b36ad79c9ded7d5c2fbefd55
NimeshKolambage <nimeshkolambage@gmail.com>
2026-04-21 00:30:47 +0530

    fixed cookies error in code

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,12 +68,15 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-
-    try{
-        res.cookie("jwt","",{maxAge:0})
-        res.status(200).json({message:"Logout successful"})
-    }catch(error){
-        res.status(500).json({message:"Server error"})
-        console.error(error,"Error in logout controller")
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "strict",
+        });
+        res.status(200).json({ message: "Logout successful" });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+        console.error(error, "Error in logout controller");
     }
-}
+};
